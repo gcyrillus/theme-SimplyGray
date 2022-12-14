1148185192826b594097c76bc9d59269d8cb3e80
gcyrillus <1832331+gcyrillus@users.noreply.github.com>
2022-12-14 09:58:17 +0100

    Update home.php

--- SimplyGray/home.php
+++ SimplyGray/home.php
@@ -17,7 +17,16 @@
 	<?php if($plxShow->plxMotor->page > 1) {echo '<script> window.onload= function() {document.getElementById("main").scrollIntoView();}</script>'; } #ne fonctionne pas sur certain navigateurs ?>
 		<ul id="posts">
 	<?php while($plxShow->plxMotor->plxRecord_arts->loop()): ?>
-			<li><?php $plxShow->artTitle('link'); ?><time datetime="<?php $plxShow->artDate('#num_year(4)-#num_month-#num_day'); ?>"><?php $plxShow->artDate('#short_month #num_year(2)'); ?></time></li>
+				<li><?php
+			// recuperation contenu pour l'afficher dans la balise title du lien
+		$dataTitle = $plxShow->plxMotor->plxRecord_arts->f('content');
+	if($dataTitle =='') $dataTitle = $plxShow->plxMotor->plxRecord_arts->f('chapo');	
+		$id = intval($plxShow->plxMotor->plxRecord_arts->f('numero'));
+        $title = plxUtils::strCheck($plxShow->plxMotor->plxRecord_arts->f('title'));
+        $url = $plxShow->plxMotor->plxRecord_arts->f('url');
+        # On affiche lien
+        echo '<a href="' . $plxShow->plxMotor->urlRewrite('?article' . $id . '/' . $url) . '" title="' . substr(strip_tags($dataTitle),0,100) . '...">' . $title . '</a>';	
+	?><time datetime="<?php $plxShow->artDate('#num_year(4)-#num_month-#num_day'); ?>"><?php $plxShow->artDate('#short_month #num_year(2)'); ?></time></li>
 	<?php endwhile; ?>
 		</ul>
 	<nav class="pagination text-center">
